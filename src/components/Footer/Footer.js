b67d46a47c7511398d3a95e1f8cf34280044cd43
itsjustaplant <alperensahin442@gmail.com>
2021-05-27 20:03:04 +0300

    feat: add auto-renew button and footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import "../../styles/Footer.css"
+
+export default function Footer() {
+    return (
+        <div className={"footer"}>
+            <span>created by</span><a href={"https://github.com/itsjustaplant"}> <strong>justaplant</strong></a><span> - devChallenges.io</span>
+        </div>
+    )
+}
